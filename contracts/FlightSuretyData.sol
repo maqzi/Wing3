7058bf9c380b83deb945f5fbc7f32d98a2742d09
maqzi <munaf.qazi@gmail.com>
2021-07-13 13:33:42 -0400

    data contract

---
+++ contracts/FlightSuretyData.sol
@@ -0,0 +1,231 @@
+pragma solidity ^0.4.25;
+
+import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+contract FlightSuretyData {
+    using SafeMath for uint256;
+
+    /********************************************************************************************/
+    /*                                       DATA VARIABLES                                     */
+    /********************************************************************************************/
+
+    address private contractOwner;                                      // Account used to deploy contract
+    bool private operational = true;                                    // Blocks all state changes throughout the contract if false
+
+    struct airline{
+        address accountAddress;                                         // Address of the airline account
+        bool isRegistered;                                              // Blocks airlines that haven't submitted a stake
+        uint256 accountValue;                                           // Monetary value of the account
+        bool isValue;                                                   // Value exists in the mapping or not
+    }
+
+    mapping(address => airline) registrations;
+    /********************************************************************************************/
+    /*                                       EVENT DEFINITIONS                                  */
+    /********************************************************************************************/
+
+
+    /**
+    * @dev Constructor
+    *      The deploying account becomes contractOwner
+    */
+    constructor
+                                (
+                                ) 
+                                public 
+    {
+        contractOwner = msg.sender;
+    }
+
+    /********************************************************************************************/
+    /*                                       FUNCTION MODIFIERS                                 */
+    /********************************************************************************************/
+
+    // Modifiers help avoid duplication of code. They are typically used to validate something
+    // before a function is allowed to be executed.
+
+    /**
+    * @dev Modifier that requires the "operational" boolean variable to be "true"
+    *      This is used on all state changing functions to pause the contract in 
+    *      the event there is an issue that needs to be fixed
+    */
+    modifier requireIsOperational() 
+    {
+        require(operational, "Contract is currently not operational");
+        _;  // All modifiers require an "_" which indicates where the function body will be added
+    }
+
+    /**
+    * @dev Modifier that requires the "ContractOwner" account to be the function caller
+    */
+    modifier requireContractOwner()
+    {
+        require(msg.sender == contractOwner, "Caller is not contract owner");
+        _;
+    }
+
+    modifier requireFunded(){
+        require(registrations[a].isRegistered, "Airline registered but hasn't submitted the stake yet");
+        _;
+    }
+
+    modifier requireAirlineExists(address a){
+        require(registrations[a].isValue, "Airline hasn't been registered yet");
+        _;
+    }
+    /********************************************************************************************/
+    /*                                       UTILITY FUNCTIONS                                  */
+    /********************************************************************************************/
+
+    /**
+    * @dev Get operating status of contract
+    *
+    * @return A bool that is the current operating status
+    */      
+    function isOperational() 
+                            public 
+                            view 
+                            returns(bool) 
+    {
+        return operational;
+    }
+
+
+    /**
+    * @dev Sets contract operations on/off
+    *
+    * When operational mode is disabled, all write transactions except for this one will fail
+    */    
+    function setOperatingStatus
+                            (
+                                bool mode
+                            ) 
+                            external
+                            requireContractOwner 
+    {
+        operational = mode;
+    }
+
+    /********************************************************************************************/
+    /*                                     SMART CONTRACT FUNCTIONS                             */
+    /********************************************************************************************/
+
+   /**
+    * @dev Add an airline to the registration queue
+    *      Can only be called from FlightSuretyApp contract
+    *
+    */   
+    function registerAirline
+                            (
+                            address add
+                            )
+                            isOperational
+                            external
+    {
+        airline newAirline = airline(add, false, 0, true);
+        registrations[add] = newAirline;
+    }
+
+
+   /**
+    * @dev Buy insurance for a flight
+    *
+    */   
+    function buy
+                            (                             
+                            )
+                            isOperational
+                            requireAirlineExists
+                            requireFunded
+                            external
+                            payable
+    {
+        require(msg.value > 0, "Invalid ether value");
+        require()
+    }
+
+    /**
+     *  @dev Credits payouts to insurees
+    */
+    function creditInsurees
+                                (
+                                    address airline,
+                                    uint value
+                                )
+                                isOperational
+                                requireAirlineExists
+                                requireFunded
+                                external
+    {
+        registrations[airline].accountValue = registrations[airline].accountValue + value;
+    }
+    
+
+    /**
+     *  @dev Transfers eligible payout funds to insuree
+     *
+    */
+    function pay
+                            (
+                            )
+                            isOperational
+                            requireAirlineExists
+                            requireFunded
+                            external
+                            payable
+    {
+        require(registrations[msg.sender].accountValue > 0, "Insufficient balance to transfer");
+
+        uint memory value = registrations[msg.sender].accountValue;
+        registrations[msg.sender].accountValue = 0;
+
+        msg.sender.transfer(value);
+    }
+
+   /**
+    * @dev Initial funding for the insurance. Unless there are too many delayed flights
+    *      resulting in insurance payouts, the contract should be self-sustaining
+    *
+    */   
+    function fund
+                            (   
+                            )
+                            isOperational
+                            requireAirlineExists
+                            public
+                            payable
+    {
+        require(!registrations[msg.sender].isRegistered, "Airline already registered");
+        require(msg.value == 10, "Payment should exactly equal 10 ethers");
+
+        registrations[msg.sender].isRegistered = true;
+    }
+
+    function getFlightKey
+                        (
+                            address airline,
+                            string memory flight,
+                            uint256 timestamp
+                        )
+                        pure
+                        internal
+                        returns(bytes32) 
+    {
+        return keccak256(abi.encodePacked(airline, flight, timestamp));
+    }
+
+    /**
+    * @dev Fallback function for funding smart contract.
+    *
+    */
+    function() 
+                            external 
+                            payable 
+    {
+        require(msg.data.length == 0, "No data allowed in the fallback function");
+        fund();
+    }
+
+
+}
+
