3616c9fb6994fa0b62662ee8aa06e8606b5ffac6
maqzi <munaf.qazi@gmail.com>
2021-07-16 04:34:50 -0400

    migrations added, packages updated, tests for oracles added

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,23 @@
+const FlightSuretyApp = artifacts.require("FlightSuretyApp");
+const FlightSuretyData = artifacts.require("FlightSuretyData");
+const fs = require('fs');
+
+module.exports = function(deployer) {
+
+    let firstAirline = '0x627306090abaB3A6e1400e9345bC60c78a8BEf57';
+    deployer.deploy(FlightSuretyData)
+    .then(() => {
+        return deployer.deploy(FlightSuretyApp, FlightSuretyData.address, firstAirline)
+                .then(() => {
+                    let config = {
+                        localhost: {
+                            url: 'http://localhost:8545',
+                            dataAddress: FlightSuretyData.address,
+                            appAddress: FlightSuretyApp.address
+                        }
+                    }
+                    fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
+                    fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
+                });
+    });
+}
